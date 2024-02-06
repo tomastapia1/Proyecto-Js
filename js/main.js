011c4b3a83389a446f193c80231888b7232af1d5
tomastapia <tomas.tapia@live.com>
2024-02-06 19:18:52 -0300

    proyecto js

--- js/main.js
+++ js/main.js
@@ -13,22 +13,26 @@ let continuarAgregando = true;
 while (continuarAgregando) {
     const seleccion = parseInt(prompt(
         "Agregar al carrito:\n1. Camara 1 - $25.000\n2. Camara 2 - $28.800\n3. Camara 3 - $32.000\n4. Camara 4 - $53.000"
-        ));
+    ));
 
     if (seleccion >= 1 && seleccion <= arrayDeCamaras.length) {
         const camaraSeleccionada = arrayDeCamaras[seleccion - 1];
-        carrito[carrito.length] = camaraSeleccionada;
+        carrito.push(camaraSeleccionada);
         total += camaraSeleccionada.precio;
+
+        let agregarOtro = prompt("Deseas agregar otra cámara? (si/no)").toLowerCase();
+
+        while (agregarOtro !== 'si' && agregarOtro !== 'no') {
+            agregarOtro = prompt("Por favor, ingresa 'si' o 'no' si deseas agregar otro producto.").toLowerCase();
+        }
+
+        if (agregarOtro !== 'si') {
+            continuarAgregando = false;
+        }
     } else {
         alert("Por favor, elija un número válido.");
     }
-
-    const agregarOtro = prompt("Deseas agregar otra cámara? (si/no)").toLowerCase();
-
-    if (agregarOtro !== 'si') {
-        continuarAgregando = false;
-    }
-};
+}
 
 mostrarCarritoYTotal();
 
@@ -37,10 +41,10 @@ function mostrarCarritoYTotal() {
 
     for (let i = 0; i < carrito.length; i++) {
         productosAgregados += carrito[i].nombre + " - Precio: " + carrito[i].precio + "\n";
-    };
+    }
 
-    productosAgregados += `Total: $${total} \nPresione aceptar para finalizar la compra.`;
+    productosAgregados += `Total: $${total} \nPresiona aceptar para finalizar la compra.`;
 
     alert(productosAgregados);
     alert('Gracias por tu compra!');
-};
+}
